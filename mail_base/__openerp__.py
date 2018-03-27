0fb30961bf2194be27d0ea1e26d2721cc44cd0aa
Ray Collazo <ray@vcloud9.com>
2018-03-27 17:54:03 -0400

    Add files via upload

---
+++ mail_base/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Mail Base",
+    "summary": """Makes Mail extendable""",
+    "category": "Discuss",
+    "images": [],
+    "version": "1.0.2",
+
+    "author": "IT-Projects LLC, Pavel Romanchenko",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info",
+    "license": "LGPL-3",
+    'price': 9.00,
+    'currency': 'EUR',
+
+    "depends": [
+        "base",
+        "mail"
+    ],
+
+    "data": [
+        "views/templates.xml",
+    ],
+    'installable': True,
+}
---
+++ mail_base/doc/changelog.rst
@@ -0,0 +1,14 @@
+`1.0.2`
+-------
+
+- **FIX**: fixed an error with unsubscribing from channel or closing dialog window
+
+`1.0.1`
+-------
+
+- **FIX**: clear messages cache on sending message via Mail Composer. Otherwise Sent, Arhives menus will have new message until user refresh whole web page
+
+`1.0.0`
+-------
+
+- Init version
